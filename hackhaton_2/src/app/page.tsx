0cef2698f01e2b4e27a05195bf4f7dcefa932cb3
Shayan-PC <MuhammadAmmarSiddiqui>
2024-12-16 22:45:56 +0500

    first commit

--- hackhaton_2/src/app/page.tsx
+++ hackhaton_2/src/app/page.tsx
@@ -1,30 +1,7 @@
-import BlueHeader from "./components/header-1";
-import Carausel from "./components/carousel";
-import Editors from "./components/editors-pick";
-import Footer from "./components/footer";
-import GreenDiv from "./components/green-man";
-import Lastdiv from "./components/end";
-import Navbar from "./components/navbar";
-import ProductCard from "./components/card-2";
-import Whitediv from "./components/white";
 
 export default function Home(){
   return(
     <div>
-      <BlueHeader/>
-      <Navbar/>
-      <Carausel/>
-      <Editors/>
-      <ProductCard/>
-      <div className="mt-5 pb-[200px]" >
-      <GreenDiv/>
-      </div>
-      <div className="pb-[50px] ">
-      <Whitediv/>
-      </div>
-      <Lastdiv/>
-      <Footer/>
-           
     </div>
   )
 }
